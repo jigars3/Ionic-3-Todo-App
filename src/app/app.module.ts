0ec6c13b8948a9945b29709281becd19938e979e
Jigar Pandya <jigarpandya@Jigars-MacBook-Pro.local>
2018-12-23 20:34:10 -0500

    Ionic 3 Todo App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {HttpClientModule} from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ArchiveTodosPage } from '../pages/archive-todos/archive-todos';
+import { TodoService } from '../providers/todo-service/todo-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ArchiveTodosPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ArchiveTodosPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController,reorderArray,ToastController } from 'ionic-angular';
+import { ArchiveTodosPage } from "../archive-todos/archive-todos";
+import { TodoService } from '../../providers/todo-service/todo-service';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,89 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  public todos = [];
+  public reorderIsEnable = false;
+  constructor(private toastController: ToastController,private todoService: TodoService,public navCtrl: NavController,private alertController: AlertController) {
+    this.todos = this.todoService.getTodos();
   }
+  toggleReorder(){
+    this.reorderIsEnable = !this.reorderIsEnable;
+  }
+  itemReordered($event){
+    reorderArray(this.todos,$event);
+  }
+  goToArchivePage(){
+    this.navCtrl.push(ArchiveTodosPage);
+  }
+  archiveTodo(todoIndex){
+    this.todoService.archivedTodo(todoIndex);
+  }
+  openTodoAlert(){
+    let addToAlert = this.alertController.create({
+      title: "Add a todo",
+      message: "Enter your todo",
+      inputs:[
+      {
+        type:"text",
+        name:"addTodoInput"
+      }],
+      buttons:[
+        {
+          text:"Cancel"
+        },
+        {
+          text:"Add Todo",
+          handler:(inputData)=>{
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todoService.addTodo(todoText);
+            addToAlert.onDidDismiss(()=>{
+              let addTodoToast = this.toastController.create({
+                message:"Todo is added",
+                duration:2000
+              });
+              addTodoToast.present();
+            })
+          }
+        }
+      ]
+    });
+    addToAlert.present();
+  }
+
+  editTodo(todoIndex){
+    let editToAlert = this.alertController.create({
+      title: "Edit a todo",
+      message: "Edit your todo",
+      inputs:[
+      {
+        type:"text",
+        name:"editTodoInput",
+        value: this.todos[todoIndex]
+      }],
+      buttons:[
+        {
+          text:"Cancel"
+        },
+        {
+          text:"Edit Todo",
+          handler:(inputData)=>{
+            let todoText;
+            todoText = inputData.editTodoInput;
+            this.todoService.editTodo(todoText,todoIndex);
+            editToAlert.onDidDismiss(()=>{
+              let editTodoToast = this.toastController.create({
+                message:"Todo is edited",
+                duration:2000
+              });
+              editTodoToast.present();
+            })
+          }
+        }
+      ]
+    });
+    editToAlert.present();
+  }
+
 
 }
